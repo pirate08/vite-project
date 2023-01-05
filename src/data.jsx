23e57e712759f195096e7cbebc5158f020185a53
Mayukh <mayukhdebgoswami82@gmail.com>
2023-01-05 20:50:56 +0530

    Data jsx has been updated...

--- src/data.jsx
+++ src/data.jsx
@@ -34,4 +34,41 @@ export default [
       image:
         'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
     },
+    {
+      id: 6,
+      name: 'Suvo Chanda',
+      age: 26,
+      image:
+        'https://plus.unsplash.com/premium_photo-1669627111428-a524797d9b97?ixlib=rb-   4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YWZyaWNhbiUyMG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      id: 7,
+      name: 'Manthan Singh',
+      age: 25,
+      image:
+      'https://images.unsplash.com/photo-1637618321985-df24e1c6cfe7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmVuZ2FsaSUyMG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      id: 8,
+      name: 'Himadri Shekhar Deb Goswami',
+      age: 26,
+      image: 
+      'https://images.unsplash.com/photo-1624440551690-0ce80688acdc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YmVuZ2FsaSUyMG1hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      id: 9,
+      name: 'Sushovan Mitra Majumdar',
+      age: 18,
+      image: 
+      'https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJlbmdhbGklMjBtYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      id: 10,
+      name: 'Mayukh Deb Goswami',
+      age: 18,
+      image: 
+      'https://images.unsplash.com/photo-1562038969-e85c13ecb2ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGJlbmdhbGklMjBtYW58ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+    },
+
+
   ];
